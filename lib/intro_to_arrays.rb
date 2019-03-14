0e31cb653a780461a3a17cfb2ab777dc0b9e6db1
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-14 20:48:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -13,11 +13,11 @@ def third_element(array)
   array[2]
 end
 def last_element(array)
-  array.last 
+  array[-1]
 end
-def first_element_with_array_methods
+def first_element_with_array_methods(array)
   array.first 
 end
-def last_element_with_array_methods
+def last_element_with_array_methods(array)
   array.last 
 end
